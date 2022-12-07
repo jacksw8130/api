d581f44f3506f221a73c794bd940939e4a40f521
jacksw8130 <118266948+jacksw8130@users.noreply.github.com>
2022-12-07 22:00:05 +0530

    s

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Server } from "./server";
 let server = new Server()
 const app = server.app;
 
-let port =  process.env.PORT || 2100;
+let port =  process.env.PORT || 2200;
 
 export const myServer = app.listen(port, ()=>{
     console.log(`Election : PORT ${port}`);
